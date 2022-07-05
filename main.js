b972dd6139df37f9464bd19a87b00e7295a77e72
Nico Blasi <nicolas.blasi.78@gmail.com>
2022-07-05 20:38:38 -0300

    corregi la posicion de la funcion para sacarla del while

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 let porcentaje = 30
+
+function descuento (precio, porcentaje){
+    return precio - (precio * porcentaje / 100)
+}
+
 do {
     let precio = parseInt(prompt("Ingresa un precio de producto mayor a $100 para aplicar descuento"))
     const socio = prompt("Sos socio?").toLocaleLowerCase()
     console.log("Es socio? " + socio)
 
     if (socio == "si") {
-        function descuento (precio, porcentaje){
-            return precio - (precio * porcentaje / 100)
-        }
+        const precioFinal = descuento (precio, porcentaje)
+        alert ("El valor con descuento es de $" + precioFinal)
+        continuar = prompt("Queres seguir calculando?").toLowerCase()    
     }
-    const precioFinal = descuento (precio, porcentaje)
-    alert ("El valor con descuento es de $" + precioFinal)
-    continuar = prompt("Queres seguir calculando?").toLowerCase()
-
 
 } while (continuar != "no")
 alert("Gracias, hasta luego!")
